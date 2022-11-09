6f1066ace6e70aa640d06f4f89aa8e20c7acf9d8
weidu_lab_63 <897131336@qq.com>
2022-11-09 16:03:27 +0800

    将部分函数移动到新建文件string_utils.py中，增加了部分函数说明，修正了部分函数中的magic number

--- utils/eval.py
+++ utils/eval.py
@@ -2,8 +2,11 @@ from collections import defaultdict
 import logging
 import sys
 
-logger = logging.getLogger(__name__)
+from requests import get
 
+from utils.calculation_utils import cal_correct_pred
+
+logger = logging.getLogger(__name__)
 
 class EvalCounts():
     """This class is evaluating counters
@@ -18,7 +21,7 @@ class EvalCounts():
         self.pred_types_cnt = defaultdict(int)
 
 
-def eval_file(file_path, eval_metrics):
+def eval_file(file_path, eval_metrics, cfg):
     """eval_file evaluates results file
 
     Args:
@@ -28,7 +31,12 @@ def eval_file(file_path, eval_metrics):
     Returns:
         tuple: results
     """
-
+    lines = []
+    lines_len = 0
+    with open(file_path, 'r') as fin:
+        for line in fin:
+            lines_len +=1
+            lines.append(line)
     with open(file_path, 'r') as fin:
         sents = []
         metric2labels = {
@@ -39,20 +47,27 @@ def eval_file(file_path, eval_metrics):
             'ent': ['Ent-True', 'Ent-Pred'],
             'rel': ['Rel-True', 'Rel-Pred'],
             'exact-rel': ['Rel-True', 'Rel-Pred'],
-            'overlap-rel': ['Rel-True', 'Rel-Pred']
+            'overlap-rel': ['Rel-True', 'Rel-Pred'],
+            'overlap-ent': ['Ent-True', 'Ent-Pred']
         }
         labels = set()
         for metric in eval_metrics:
             labels.update(metric2labels[metric])
         label2idx = {label: idx for idx, label in enumerate(labels)}
         sent = [[] for _ in range(len(labels))]
+        idx = 0
         for line in fin:
             line = line.strip('\r\n')
             if line == "":
                 sents.append(sent)
                 sent = [[] for _ in range(len(labels))]
             else:
-                words = line.split('\t')
+                words = line
+                k = idx
+                while k+1 < lines_len and len(lines[k+1]) != 0 and lines[k+1][0] == ' ':
+                    words += lines[k+1].strip('\r\n')
+                    k+=1
+                words = words.split('\t')
                 if words[0] in ['Sequence-Label-True', 'Sequence-Label-Pred', 'Joint-Label-True', 'Joint-Label-Pred']:
                     sent[label2idx[words[0]]].extend(words[1].split(' '))
                 elif words[0] in ['Separate-Position-True', 'Separate-Position-Pred']:
@@ -60,15 +75,20 @@ def eval_file(file_path, eval_metrics):
                 elif words[0] in ['Ent-Span-Pred']:
                     sent[label2idx[words[0]]].append(eval(words[1]))
                 elif words[0] in ['Ent-True', 'Ent-Pred']:
-                    sent[label2idx[words[0]]].append([words[1], eval(words[2])])
+                    sent[label2idx[words[0]]].append([words[1], eval(words[2]), words[3]])
                 elif words[0] in ['Rel-True', 'Rel-Pred']:
-                    sent[label2idx[words[0]]].append([words[1], eval(words[2]), eval(words[3])])
+                    sent[label2idx[words[0]]].append([  words[1], 
+                                                        eval(words[2]), 
+                                                        eval(words[3]), 
+                                                        words[4],
+                                                        words[5]])
+            idx+=1
         sents.append(sent)
 
     counts = {metric: EvalCounts() for metric in eval_metrics}
 
     for sent in sents:
-        evaluate(sent, counts, label2idx)
+        evaluate(sent, counts, label2idx, cfg)
 
     results = []
 
@@ -85,7 +105,7 @@ def eval_file(file_path, eval_metrics):
     return results
 
 
-def evaluate(sent, counts, label2idx):
+def evaluate(sent, counts, label2idx, cfg):
     """evaluate calculates counters
     
     Arguments:
@@ -143,14 +163,14 @@ def evaluate(sent, counts, label2idx):
     correct_ent2idx = defaultdict(set)
     correct_span2ent = dict()
     correct_span = set()
-    for ent, span in sent[label2idx['Ent-True']]:
+    for ent, span, _ in sent[label2idx['Ent-True']]:
         correct_span.add(span)
         correct_span2ent[span] = ent
         correct_ent2idx[ent].add(span)
 
     pred_ent2idx = defaultdict(set)
     pred_span2ent = dict()
-    for ent, span in sent[label2idx['Ent-Pred']]:
+    for ent, span, _ in sent[label2idx['Ent-Pred']]:
         pred_span2ent[span] = ent
         pred_ent2idx[ent].add(span)
 
@@ -170,17 +190,19 @@ def evaluate(sent, counts, label2idx):
             pred_correct_cnt = len(correct_ent2idx[ent] & pred_ent2idx[ent])
             counts['ent'].pred_correct_cnt += pred_correct_cnt
             counts['ent'].pred_correct_types_cnt[ent] += pred_correct_cnt
+            
+            
 
     # evaluate relation
     if 'rel' in counts:
         correct_rel2idx = defaultdict(set)
-        for rel, span1, span2 in sent[label2idx['Rel-True']]:
+        for rel, span1, span2, _, _ in sent[label2idx['Rel-True']]:
             if span1 not in correct_span2ent or span2 not in correct_span2ent:
                 continue
             correct_rel2idx[rel].add((span1, span2))
 
         pred_rel2idx = defaultdict(set)
-        for rel, span1, span2 in sent[label2idx['Rel-Pred']]:
+        for rel, span1, span2, _, _ in sent[label2idx['Rel-Pred']]:
             if span1 not in pred_span2ent or span2 not in pred_span2ent:
                 continue
             pred_rel2idx[rel].add((span1, span2))
@@ -194,36 +216,57 @@ def evaluate(sent, counts, label2idx):
             pred_correct_rel_cnt = len(correct_rel2idx[rel] & pred_rel2idx[rel])
             counts['rel'].pred_correct_cnt += pred_correct_rel_cnt
             counts['rel'].pred_correct_types_cnt[rel] += pred_correct_rel_cnt
+
+    # overlap entity evaluation
+    '''
+    correct_span2ent: the span of entities to the type of entities.
+    
+    overlap realtion evaluation only pay attention to the text and the typy of the entities.
+    '''
+    
+    if 'overlap-ent' in counts:
+        overlap_correct_ent2idx = defaultdict(set)
+        for ent, span, text in sent[label2idx['Ent-True']]:
+            if span not in correct_span2ent:
+                continue
+            # overlap_correct_rel2idx[rel].add((  text1,  
+            #                                     text2, ))
+            overlap_correct_ent2idx[ent].add((text, correct_span2ent[span]))
+            # overlap_correct_rel2idx[rel].add((  correct_span2ent[span1], 
+            #                                     correct_span2ent[span2]))
+
+        overlap_pred_ent2idx = defaultdict(set)
+        for ent, span, text in sent[label2idx['Ent-Pred']]:
+            if span not in pred_span2ent:
+                continue
+            overlap_pred_ent2idx[ent].add(( text, pred_span2ent[span]))
+            # overlap_pred_rel2idx[rel].add(( text1,  
+            #                                 text2, ))
+            # overlap_pred_rel2idx[rel].add(( pred_span2ent[span1], 
+            #                                 pred_span2ent[span2]))
+        all_overlap_ents = set(overlap_correct_ent2idx) | set(overlap_pred_ent2idx)
+                
+        for ent in all_overlap_ents:
+            counts['overlap-ent'].correct_cnt += len(overlap_correct_ent2idx[ent])
+            counts['overlap-ent'].correct_types_cnt[ent] += len(overlap_correct_ent2idx[ent])
+            counts['overlap-ent'].pred_cnt += len(overlap_pred_ent2idx[ent])
+            counts['overlap-ent'].pred_types_cnt[ent] += len(overlap_pred_ent2idx[ent])
+            
+            # overlap_pred_correct_rel_cnt = len(overlap_correct_rel2idx[rel] & overlap_pred_rel2idx[rel])
+            overlap_pred_correct_ent_cnt = cal_correct_pred(overlap_correct_ent2idx[ent], 
+                                                            overlap_pred_ent2idx[ent], 
+                                                            cfg.overlap_match_rate,
+                                                            'ent')
+            counts['overlap-ent'].pred_correct_cnt += overlap_pred_correct_ent_cnt
+            counts['overlap-ent'].pred_correct_types_cnt[ent] += overlap_pred_correct_ent_cnt
+            
+            
     # overlap relation evaluation
     '''
     correct_span2ent: the span of entities to the type of entities.
     
     overlap realtion evaluation only pay attention to the text and the typy of the entities.
     '''
-    def get_overlap(s1, s2):
-        s = difflib.SequenceMatcher(None, s1, s2)
-        pos_a, pos_b , size = s.find_longest_match(0, len(s1), 0, len(s2))
-        return size
-    def cal(correct_set, pred_set, p = cfg.overlap_match_rate):
-        vis = []
-        cnt = 0
-        for pred in pred_set:
-            for idx, correct in enumerate(correct_set):
-
-                if pred[1] != correct[1] or pred[3] != correct[3]:
-                    continue
-                if idx in vis:
-                    continue
-                is_cnt = True
-                if get_overlap(pred[0].replace(' ', ''), correct[0].replace(' ', '')) < p*len(correct[0].replace(' ', '')):
-                    is_cnt = False
-                if get_overlap(pred[2].replace(' ', ''), correct[2].replace(' ', '')) < p*len(correct[2].replace(' ', '')):
-                    is_cnt = False
-                if is_cnt:
-                    cnt += 1
-                    vis.append(idx)
-                    break
-        return cnt
 
     if 'overlap-rel' in counts:
         overlap_correct_rel2idx = defaultdict(set)
@@ -256,20 +299,23 @@ def evaluate(sent, counts, label2idx):
             counts['overlap-rel'].pred_types_cnt[rel] += len(overlap_pred_rel2idx[rel])
             
             # overlap_pred_correct_rel_cnt = len(overlap_correct_rel2idx[rel] & overlap_pred_rel2idx[rel])
-            overlap_pred_correct_rel_cnt = cal(overlap_correct_rel2idx[rel], overlap_pred_rel2idx[rel])
+            overlap_pred_correct_rel_cnt = cal_correct_pred(overlap_correct_rel2idx[rel], 
+                                                            overlap_pred_rel2idx[rel],
+                                                            cfg.overlap_match_rate,
+                                                            'rel')
             counts['overlap-rel'].pred_correct_cnt += overlap_pred_correct_rel_cnt
             counts['overlap-rel'].pred_correct_types_cnt[rel] += overlap_pred_correct_rel_cnt
-            
+
     # exact relation evaluation
     if 'exact-rel' in counts:
         exact_correct_rel2idx = defaultdict(set)
-        for rel, span1, span2 in sent[label2idx['Rel-True']]:
+        for rel, span1, span2, _, _ in sent[label2idx['Rel-True']]:
             if span1 not in correct_span2ent or span2 not in correct_span2ent:
                 continue
             exact_correct_rel2idx[rel].add((span1, correct_span2ent[span1], span2, correct_span2ent[span2]))
 
         exact_pred_rel2idx = defaultdict(set)
-        for rel, span1, span2 in sent[label2idx['Rel-Pred']]:
+        for rel, span1, span2, text1, text2 in sent[label2idx['Rel-Pred']]:
             if span1 not in pred_span2ent or span2 not in pred_span2ent:
                 continue
             exact_pred_rel2idx[rel].add((span1, pred_span2ent[span1], span2, pred_span2ent[span2]))
@@ -302,7 +348,7 @@ def report(counts):
     logger.info("recall: {:6.2f}%".format(100 * r))
     logger.info("f1: {:6.2f}%".format(100 * f))
 
-    score = f
+    score = (p, r, f)
 
     for type in counts.pred_correct_types_cnt:
         p, r, f = calculate_metrics(counts.pred_correct_types_cnt[type], counts.pred_types_cnt[type],
